772b89163c553af8a4043eb915c787c655294e37
realdev <realdev.company@gmail.com>
2025-05-31 19:42:32 +0200

    Streaming for discussions

--- composeApp/src/commonMain/kotlin/cloud/realdev/myai/views/viewmodels/DiscussionViewModel.kt
+++ composeApp/src/commonMain/kotlin/cloud/realdev/myai/views/viewmodels/DiscussionViewModel.kt
@@ -6,18 +6,25 @@ import cloud.realdev.myai.models.discuss.DiscussionRequest
 import cloud.realdev.myai.models.discuss.DiscussionResult
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.timeout
+import io.ktor.client.request.accept
 import io.ktor.client.request.post
+import io.ktor.client.request.preparePost
 import io.ktor.client.request.setBody
+import io.ktor.client.statement.HttpStatement
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
+import io.ktor.utils.io.ByteReadChannel
 import io.ktor.utils.io.InternalAPI
+import io.ktor.utils.io.readUTF8Line
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json
 
 class DiscussionViewModel: ViewModel() {
 
@@ -30,7 +37,7 @@ class DiscussionViewModel: ViewModel() {
     private val _sendingRequest = MutableStateFlow(false)
     val sendingRequest: StateFlow<Boolean> = _sendingRequest.asStateFlow()
 
-    val client = HttpClient {
+    val client = HttpClient(CIO) {
         install(ContentNegotiation) {
             json()
         }
@@ -57,8 +64,9 @@ class DiscussionViewModel: ViewModel() {
         try {
             viewModelScope.launch {
                 try {
-                    val response = client.post("http://192.168.1.25:8000/discuss") {
+                    val httpStatement: HttpStatement = client.preparePost("http://192.168.1.25:8000/discuss") {
                         contentType(ContentType.Application.Json)
+                        accept(ContentType.Text.EventStream)
                         setBody(_discussionRequest.value)
                         timeout {
                             requestTimeoutMillis = 60000
@@ -66,8 +74,24 @@ class DiscussionViewModel: ViewModel() {
                         }
                     }
 
-                    val translationResult = response.body<DiscussionResult>()
-                    _discussionResult.value = translationResult
+                    httpStatement.execute { response ->
+                        val channel: ByteReadChannel = response.body()
+                        while (!channel.isClosedForRead) {
+                            val line = channel.readUTF8Line()
+                            if (line != null && line.isNotEmpty()) {
+                                try {
+                                    val json = Json.decodeFromString<DiscussionResult>(line)
+                                    if(_discussionResult.value == null) {
+                                        _discussionResult.value = json
+                                        continue
+                                    }
+                                    _discussionResult.value = _discussionResult.value?.copy(response = _discussionResult.value?.response + json.response)
+                                } catch (e: Exception) {
+                                    print(e)
+                                }
+                            }
+                        }
+                    }
                     _sendingRequest.value = false
                 } catch (e: Exception) {
                     print(e)
