c8472997a4e2159c6052f0f043a696b39afe585b
realdev <realdev.company@gmail.com>
2025-10-10 15:21:41 +0100

    version with icon

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -58,7 +58,7 @@ android {
         applicationId = "cloud.realdev.myai"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
     }
     packaging {
