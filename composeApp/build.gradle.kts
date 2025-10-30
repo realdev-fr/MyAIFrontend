bac59dd0a32b6c800ecb122340e61f912fee0e10
realdev <realdev.company@gmail.com>
2025-10-30 23:00:34 +0100

    Change version code

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -58,7 +58,7 @@ android {
         applicationId = "cloud.realdev.myai"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
     }
     packaging {
